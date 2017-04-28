068147b7bfe8584fc5d57efc6b7731c49475dd31
Nazariy Mokriy <nazarriko@gmail.com>
2017-04-28 08:32:52 +0300

    up

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,6 @@
 $(function () {
 
-	function initView () {
+	function initView() {
 		$('.js-view-slider').owlCarousel({
 			items: 4,
 			dots: false,
@@ -23,6 +23,42 @@ $(function () {
 		dots: true,
 		smartSpeed: '600',
 		dotClass: 'bar__slider-dot'
+	});
+
+	$('.js-testimonials-slider').owlCarousel({
+		onInitialized: function () {
+			$('.js-testimonials-slider .owl-item.center').css({
+				transform: "scale(1)"
+			});
+			// $(".js-testimonials-slider .owl-item").css({
+			// 	transform:"none"
+			// });
+			$(".js-testimonials-slider .owl-item.active:not('.owl-item.center')").css({
+				transform: "scale(.7)"
+			});
+		},
+		onTranslated: function () {
+			// $(".js-testimonials-slider .owl-item").css({
+			// 	transform:"none"
+			// });
+			$(".js-testimonials-slider .owl-item.active").css({
+				transform: "scale(.7)"
+			});
+			$(".js-testimonials-slider .owl-item.center").css({
+				transform: "scale(1)"
+			});
+		},
+		loop: true,
+		center: true,
+		items: 3,
+		dots: false,
+		nav: true,
+		smartSpeed: '600',
+		navContainer: 'testimonials__slider-nav',
+		navText: [
+			'<button type="button" class="testimonials__slider-arr testimonials__slider-arr_prev"><svg class="view__slider-icon icon-arr-sld_l"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-arr-sld_l"></use></svg></button>',
+			'<button type="button" class="testimonials__slider-arr testimonials__slider-arr_next"><svg class="view__slider-icon icon-arr-sld_r"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-arr-sld_r"></use></svg></button>'
+		]
 	});
 
 });
