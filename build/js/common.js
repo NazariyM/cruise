5b81f22b2c92e5510d8fdbf2ffe6927ac1af7db6
Nazariy Mokriy <nazarriko@gmail.com>
2017-04-29 15:23:15 +0300

    upd!

---
+++ build/js/common.js
@@ -0,0 +1,31 @@
+'use strict';
+
+$(function () {
+	//excursion tabs
+	function initExcursionToggling() {
+		var $tabsButton = $('.js-excursion-tabs-nav').find('li');
+		var $tabsContent = $('.js-excursion-tabs-data').children('li');
+
+		$tabsContent.hide();
+
+		$tabsButton.each(function () {
+			if ($(this).hasClass('is-active')) {
+				$(this).trigger('click');
+				$tabsContent.eq($(this).index()).fadeIn(600);
+			}
+		});
+
+		$tabsButton.on('click', function (e) {
+			e.preventDefault();
+			var $index = $(this).index();
+			var $_this = $(this);
+
+			$_this.hasClass('is-active') ? $_this.removeClass('is-active') : $_this.addClass('is-active').siblings().removeClass('is-active');
+
+			$tabsContent.eq($index).fadeToggle(600);
+			$tabsContent.hide().eq($index).fadeToggle(600);
+		});
+	}
+
+	initExcursionToggling();
+});
---
+++ build/js/components/sliders.js
@@ -0,0 +1,61 @@
+'use strict';
+
+$(function () {
+
+	function initView() {
+		$('.js-view-slider').owlCarousel({
+			mouseDrag: false,
+			items: 4,
+			dots: false,
+			nav: true,
+			margin: 5,
+			smartSpeed: '600',
+			navText: ['<button type="button" class="view__slider-arr view__slider-arr_prev"><svg class="view__slider-icon icon-arr-sld_l"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-arr-sld_l"></use></svg></button>', '<button type="button" class="view__slider-arr view__slider-arr_next"><svg class="view__slider-icon icon-arr-sld_r"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-arr-sld_r"></use></svg></button>'],
+			responsive: {
+				320: {
+					items: 1
+				},
+				499: {
+					items: 2
+				},
+				767: {
+					items: 4
+				}
+			}
+		});
+
+		$('[data-fancybox]').fancybox();
+	}
+
+	initView();
+
+	$('.js-bar-slider').owlCarousel({
+		items: 1,
+		dots: true,
+		smartSpeed: '600',
+		dotClass: 'bar__slider-dot'
+	});
+
+	$('.js-testimonials-slider').slick({
+		centerMode: true,
+		focusOnSelect: true,
+		useTransform: true,
+		centerPadding: '25%',
+		cssEase: 'ease-in-out',
+		variableHeight: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: true,
+		dots: false,
+		arrows: true,
+		prevArrow: '<button type="button" class="testimonials__slider-arr testimonials__slider-arr_prev"><svg class="testimonials__slider-icon icon-arr-sld_l"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-arr-sld_l"></use></svg></button>',
+		nextArrow: '<button type="button" class="testimonials__slider-arr testimonials__slider-arr_next"><svg class="testimonials__slider-icon icon-arr-sld_r"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-arr-sld_r"></use></svg></button>',
+		responsive: [{
+			breakpoint: 767,
+			settings: {
+				centerMode: false,
+				centerPadding: 0
+			}
+		}]
+	});
+});
